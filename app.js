5f40dcddbcd51dd54fb8f1108f6d49fa308863e5
Yusuf Salih <86113227+hbkysf27@users.noreply.github.com>
2022-11-11 10:41:25 +0530

    Read Env variables create .env file and store api string to read it anywhere in the project

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 
+require('dotenv/config');
+const api = process.env.API_URL;
+
 //initialize routes
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get(api + '/products', (req, res) => {
+    res.send('Hello API!');
 })
 
 //initialize ports
 app.listen(3000, () => {
+    console.log(api);
     console.log('listening on port 3000');
 })
