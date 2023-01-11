11316179a40a923cf743252c4b0314f219afc5f8
Yusuf <Yusufsalih4@gmail.com>
2023-01-11 15:06:30 +0530

    logging api requests

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan=require('morgan');
+const mongoose = require('mongoose');
 
 require('dotenv/config');
 const api = process.env.API_URL;
 
+//middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+
 //initialize routes
-app.get(api + '/products', (req, res) => {
-    res.send('Hello API!');
+app.get(`${api}/products`, (req, res) =>{
+    const product= {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some url',
+    }
+    res.send(product);
 })
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct=req.body;
+    console.log(newProduct);
+    res.send(newProduct); 
+    })
+
+
+
 
 //initialize ports
 app.listen(3000, () => {
-    console.log(api);
-    console.log('listening on port 3000');
+    
+    console.log('server is running in http://localhost:3000');
 })
