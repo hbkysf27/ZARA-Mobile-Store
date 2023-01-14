44709ad1873dad59a3ef415e3e0289df75e21013
Yusuf <Yusufsalih4@gmail.com>
2023-01-14 11:20:33 +0530

    create backend API Routes % Schemas for products

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const morgan=require('morgan');
+const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
-require('dotenv/config');
-const api = process.env.API_URL;
+//require('dotenv/config');
+require('dotenv').config();
+
+
+app.use(cors());
+app.options('*', cors())
 
 //middleware
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-//initialize routes
-app.get(`${api}/products`, (req, res) =>{
-    const product= {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some url',
-    }
-    res.send(product);
-})
 
-app.post(`${api}/products`, (req, res) => {
-    const newProduct=req.body;
-    console.log(newProduct);
-    res.send(newProduct); 
-    })
+//Routes
 
+const productsRoutes = require('../routes/products');
+
+const api = process.env.API_URL;
+
+
+app.use(`${api}/products`, productsRoutes);
+
+
+
+mongoose.set('strictQuery', false);
+//Database
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'zara-database'
-})
-.then(() => {
-    console.log('Database connection established')
+    dbName: "zara-database",
+  })
+.then(()=>{
+    console.log('Database Connection is ready...')
 })
 .catch((err)=> {
     console.log(err);
 })
 
+//Server
+app.listen(3000, ()=>{
 
-//initialize ports
-app.listen(3000, () => {
-    
-    console.log('server is running in http://localhost:3000');
+    console.log('server is running http://localhost:3000');
 })
